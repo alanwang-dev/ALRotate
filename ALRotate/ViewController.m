db8b6c75cc3b6a3d6520c3b46d2d3b5995a0430a
wangkecheng <wangkecheng@kingsoft.com>
2017-03-17 18:29:27 +0800

    add rotate demo :how to stay a view in UIViewContrller which autoRotate mode is YES

--- ALRotate/ViewController.m
+++ ALRotate/ViewController.m
@@ -1,14 +1,27 @@
 //
 //  ViewController.m
-//  ALRotate
+//  ALAutoLayout
 //
-//  Created by iVermisseDich on 2017/3/17.
+//  Created by iVermisseDich on 2017/3/16.
 //  Copyright © 2017年 com.ksyun. All rights reserved.
 //
 
 #import "ViewController.h"
+#import <libksygpulive/libksygpulive.h>
+#import "KSYGPUStreamerkit.h"
+
+#define kScreenWidth        [UIScreen mainScreen].bounds.size.width
+#define kScreenHeight       [UIScreen mainScreen].bounds.size.height
+
+#define kSystemVersion      [[[UIDevice currentDevice] systemVersion] floatValue]
+#define IOS8_OR_LATER       (kSystemVersion >= 8)
+
+// How To Stay a View in UIViewController Which AutoRotateMode is YES
 
 @interface ViewController ()
+
+@property (nonatomic) UIView *bgView;
+@property (nonatomic) KSYGPUStreamerKit *kit;
 
 @end
 
@@ -16,14 +29,130 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    _kit = [[KSYGPUStreamerKit alloc] initWithDefaultCfg];
+    
+    _bgView = [[UIView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:_bgView];
+    
+    [_kit startPreview:_bgView];
+}
+
+#pragma mark - UIViewController Rotation
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration{
+    [super willRotateToInterfaceOrientation:toInterfaceOrientation duration:duration];
+    
+    // size
+    CGFloat minLength = MIN(kScreenWidth, kScreenHeight);
+    CGFloat maxLength = MAX(kScreenWidth, kScreenHeight);
+    CGRect newFrame;
+    
+    // frame
+    CGAffineTransform newTransform;
+    // need stay frame after animation
+    CGAffineTransform newTransformOfStay;
+    // whether need to stay
+    BOOL needStay;
+    
+    if (toInterfaceOrientation == UIInterfaceOrientationPortrait) {
+        newTransform = CGAffineTransformIdentity;
+        newFrame = CGRectMake(0, 0, minLength, maxLength);
+    } else {
+        if (self.interfaceOrientation == UIInterfaceOrientationPortrait) {
+            newTransform = CGAffineTransformMakeRotation(M_PI_2*(toInterfaceOrientation == UIInterfaceOrientationLandscapeLeft ? 1 : -1));
+        } else {
+            needStay = YES;
+            if (self.interfaceOrientation == UIInterfaceOrientationLandscapeLeft) {
+                newTransform = CGAffineTransformRotate(_bgView.transform,M_PI * 1.00001);
+                newTransformOfStay = CGAffineTransformRotate(_bgView.transform, M_PI);
+            }else{
+                newTransform = CGAffineTransformRotate(_bgView.transform,IOS8_OR_LATER ? 1.00001 * M_PI : M_PI * 0.99999);
+                newTransformOfStay = CGAffineTransformRotate(_bgView.transform, M_PI);
+                
+            }
+        }
+        newFrame = CGRectMake(0, 0, maxLength, minLength);
+    }
+    
+    __weak typeof(self) weakSelf = self;
+    [UIView animateWithDuration:duration animations:^{
+        __strong __typeof(weakSelf) strongSelf = weakSelf;
+        if (!strongSelf) {
+            return ;
+        }
+        strongSelf.bgView.transform = newTransform;
+        strongSelf.bgView.frame = newFrame;
+    }completion:^(BOOL finished) {
+        __strong __typeof(weakSelf)strongSelf = weakSelf;
+        if (!strongSelf) {
+            return ;
+        }
+        strongSelf.bgView.frame = strongSelf.bgView.superview.bounds;
+    }];
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - iOS_8_OR_LATER
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator{
+    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+    
+    // size
+    CGFloat minLength = MIN(kScreenWidth, kScreenHeight);
+    CGFloat maxLength = MAX(kScreenWidth, kScreenHeight);
+    CGRect newFrame;
+    
+    // frame
+    CGAffineTransform newTransform;
+    // need stay frame after animation
+    CGAffineTransform newTransformOfStay;
+    // whether need to stay
+    BOOL needStay;
+    
+    UIInterfaceOrientation currentInterfaceOrientation = [UIApplication sharedApplication].statusBarOrientation;
+    UIDeviceOrientation toDeviceOrientation = [UIDevice currentDevice].orientation;
+    
+    if (toDeviceOrientation == UIDeviceOrientationPortrait) {
+        newTransform = CGAffineTransformIdentity;
+        newFrame = CGRectMake(0, 0, minLength, maxLength);
+    } else {
+        if (currentInterfaceOrientation == UIInterfaceOrientationPortrait) {
+            newTransform = CGAffineTransformMakeRotation(M_PI_2*(toDeviceOrientation == UIDeviceOrientationLandscapeRight ? 1 : -1));
+        } else {
+            needStay = YES;
+            if (currentInterfaceOrientation == UIInterfaceOrientationLandscapeLeft) {
+                newTransform = CGAffineTransformRotate(_bgView.transform, M_PI * 1.00001);
+                newTransformOfStay = CGAffineTransformRotate(_bgView.transform, M_PI);
+            }else{
+                newTransform = CGAffineTransformRotate(_bgView.transform, IOS8_OR_LATER ? 1.00001 * M_PI : M_PI * 0.99999);
+                newTransformOfStay = CGAffineTransformRotate(_bgView.transform, M_PI);
+            }
+        }
+        newFrame = CGRectMake(0, 0, maxLength, minLength);
+    }
+    
+    __weak typeof(self) weakSelf = self;
+    [coordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
+        __strong __typeof(weakSelf) strongSelf = weakSelf;
+        if (!strongSelf) {
+            return ;
+        }
+        strongSelf.bgView.transform = newTransform;
+        strongSelf.bgView.frame =  newFrame;
+    } completion:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
+        __strong __typeof(weakSelf)strongSelf = weakSelf;
+        if (!strongSelf) {
+            return ;
+        }
+        if (needStay) {
+            strongSelf.bgView.transform = newTransformOfStay;
+        }
+        strongSelf.bgView.frame = strongSelf.bgView.superview.bounds;
+    }];
 }
 
+- (BOOL)shouldAutorotate{
+    return YES;
+}
 
 @end
---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '7.0'
+inhibit_all_warnings!
+use_frameworks!
+
+target ‘ALRotate’ do
+	pod 'GPUImage'
+	pod 'libksygpulive'
+end
